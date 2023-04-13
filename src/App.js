68ee40d21d63351d8402055c986e7bf97b077fbf
khedim <oussamakhedim79@gmail.com>
2023-04-13 14:59:04 +0100

    adding some comments

--- src/App.js
+++ src/App.js
@@ -13,11 +13,14 @@ function App() {
       id="chatBot"
       className="App position-fixed start-0 bottom-0 ms-sm-3 mb-sm-3"
     >
+      {/* if true it displays the chatBot else it displays the the ball */}
       {showChatBot ? (
         <div
           id="chat-bot"
           className="bg-light rounded-5 overflow-hidden d-flex flex-column"
         >
+          {/* if there is not a first name it displays the form section 
+            else it displays the chatBot section */}
           {firstname === "" ? (
             <ChatBotForm
               setShowChatBot={setShowChatBot}
